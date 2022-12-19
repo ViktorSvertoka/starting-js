9ffe31343d46b4b83d9befccfe52a9f54950519e
Viktor Svertoka <victor.svertoka@gmail.com>
2022-12-19 22:55:10 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// переменные и типы примитивов
+// const и let
+// логирование с методом console.log()
+// уникальность идентификатора
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Добро пожаловать!';
+const isOpen = true;
+const sholdConfirm = false;
+
+console.log(totalPrice);

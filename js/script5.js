0738563cb84564b3494708dc1000c0236c419487
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-23 19:07:25 +0200

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -1,7 +1,3 @@
-function checkForSpam(message) {
-  let result;
-  // Change code below this line
-  result = message.includes(spam);
-  // Change code above this line
-  return result;
-}
+const fruits = ['apple', 'peach', 'pear', 'banana'];
+const lastElementIndex = fruits.length - 1;
+const lastElement = [3];

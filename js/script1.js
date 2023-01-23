6994e0fa147853092edc813c47dd908ca76cfbf1
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-23 19:19:34 +0200

    changes

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,1 @@
-// переменные и типы примитивов
-// const и let
-// логирование с методом console.log()
-// уникальность идентификатора
 
-const age = 10;
-const totalPrice = 200.74;
-const userName = 'Chelsy';
-const message = 'Добро пожаловать!';
-const isOpen = true;
-const sholdConfirm = false;
-
-console.log('Цена', totalPrice);

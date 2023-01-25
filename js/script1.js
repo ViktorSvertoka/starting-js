0c471dc1b5658673b3dfd8900dce0363fedb686f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-26 00:23:02 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,17 @@
-`use strict`;
+function calculateEngravingPrice(message, pricePerWord) {
+  // Change code below this line
+  if ((message === 'JavaScript is in my blood', pricePerWord === 10)) {
+    return 50;
+  } else if (
+    (message === 'Web-development is creative work', pricePerWord === 20)
+  ) {
+    return 80;
+  } else if ((message === 'JavaScript is in my blood', pricePerWord === 20)) {
+    return 100;
+  } else if (
+    (message === 'Web-development is creative work', pricePerWord === 40)
+  ) {
+    return 160;
+  }
+  // Change code above this line
+}

c7ec0cf666821ab095bd4edcb2f65cd4888e9c05
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-30 22:37:10 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -18,10 +18,3 @@ function makeArray(firstArray, secondArray, maxLength) {
   }
   return newArray;
 }
-
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));

7ce4678d63de60e0737c8737971a83875931b685
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-31 00:17:04 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,11 +1,11 @@
 const apartment = {
   imgUrl: 'https://via.placeholder.com/640x480',
   descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
   owner: {
-    name: 'Henry',
+    name: 'Henry Sibola',
     phone: '982-126-1588',
     email: 'henry.carter@aptmail.com',
   },
@@ -13,7 +13,8 @@ const apartment = {
 
 // Change code below this line
 
-apartment.price = 5000;
-apartment.rating = 4.7;
-apartment.owner.name = 'Henry Sibola';
-apartment.tags.push('trusted');
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {};
+apartment.location.country = 'Jamaica';
+apartment.location.city = 'Kingston';

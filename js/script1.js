3179a0d9069d4b6c3d9dc8f429c0e91fcb391013
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-30 23:56:20 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -4,11 +4,16 @@ const apartment = {
   rating: 4,
   price: 2153,
   tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
 };
 
 // Change code below this line
-const aptRating = apartment['rating'];
-const aptDescr = apartment['descr'];
-const aptPrice = apartment['price'];
-const aptTags = apartment['tags'];
-// Change code above this line
+
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = 'Henry Sibola';
+apartment.tags.push('trusted');

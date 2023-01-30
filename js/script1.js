bee5192ae3ed9ac97093842b6c8a2c861a25897c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-31 00:23:31 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,20 +1,13 @@
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4.7,
-  price: 5000,
-  tags: ['premium', 'promoted', 'top', 'trusted'],
-  owner: {
-    name: 'Henry Sibola',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
+const name = 'Repair Droid';
+const price = 2500;
+const image = 'https://via.placeholder.com/640x480';
+const tags = ['on sale', 'trending', 'best buy'];
+
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
+  // Change code above this line
 };
-
-// Change code below this line
-
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = {};
-apartment.location.country = 'Jamaica';
-apartment.location.city = 'Kingston';

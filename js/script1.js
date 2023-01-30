7e3332d311f00eee85e129cac4e30a036224326a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-30 23:20:16 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -4,18 +4,11 @@ const apartment = {
   rating: 4,
   price: 2153,
   tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Henry',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
 };
 
 // Change code below this line
-const ownerName = apartment.owner.name;
-const ownerPhone = apartment.owner.phone;
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[2];
+const aptRating = apartment['rating'];
+const aptDescr = apartment['descr'];
+const aptPrice = apartment['price'];
+const aptTags = apartment['tags'];
 // Change code above this line

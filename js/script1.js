16b3c5539378f5ea845eef76191865127742878a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-31 21:31:15 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,9 @@
-const keys = [];
-const values = [];
-const advert = {
-  service: 'apt',
-};
-const apartment = Object.create(advert);
-apartment.descr = 'Spacious apartment in the city center';
-apartment.rating = 4;
-apartment.price = 2153;
-
-for (const key in apartment) {
-  // Change code below this line
-  if (apartment.hasOwnProperty(key)) {
-    keys.push(key);
-    values.push(apartment[key]);
+function countProps(object) {
+  let propCount = 0;
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      propCount++;
+    }
   }
-  // Change code above this line
+  return propCount;
 }

d9e7c4d93d3a166bd0356288da0dd2475529cbef
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-01 17:32:53 +0200

    Update script1.js
    Co-Authored-By: Сергій Євчишен <116813260+Sergiy-Yevchihen@users.noreply.github.com>

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,11 @@
-const scores = [89, 64, 42, 17, 93, 51, 26];
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
 // Change code below this line
-const bestScore = Math.max(...scores);
-const worstScore = Math.min(...scores);
+const allScores = [
+  ...firstGroupScores,
+  ...secondGroupScores,
+  ...thirdGroupScores,
+];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);

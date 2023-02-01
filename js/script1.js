db8cf1b16570d1e1b00533c43eef683b6fe6f180
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-01 21:29:21 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,17 @@
-function findMatches(array, ...values) {
-  const matches = [];
-  for (let value of values) {
-    if (array.indexOf(value) !== -1) {
-      matches.push(value);
-    }
-  }
-  return matches;
-}
+const bookShelf = {
+  // Change code below this line
+  books: ['The last kingdom', 'The guardian of dreams'],
+  getBooks() {
+    return 'Returning all books';
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  },
+  // Change code above this line
+};

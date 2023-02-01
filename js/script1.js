49459a4db387a9359c3b945378aaed6994da45e8
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-01 15:26:09 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,15 +1,11 @@
-const colors = [
-  { hex: '#f44336', rgb: '244,67,54' },
-  { hex: '#2196f3', rgb: '33,150,243' },
-  { hex: '#4caf50', rgb: '76,175,80' },
-  { hex: '#ffeb3b', rgb: '255,235,59' },
-];
-
-const hexColors = [];
-const rgbColors = [];
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
 // Change code below this line
 
-for (let color of colors) {
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb);
-}
+const { yesterday, today, tomorrow } = highTemperatures;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;

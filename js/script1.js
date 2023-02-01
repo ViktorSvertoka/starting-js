29ad98cf6641d733fae5876ef996269b289e6e86
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-01 16:50:56 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,15 +1,32 @@
-const colors = [
-  { hex: '#f44336', rgb: '244,67,54' },
-  { hex: '#2196f3', rgb: '33,150,243' },
-  { hex: '#4caf50', rgb: '76,175,80' },
-  { hex: '#ffeb3b', rgb: '255,235,59' },
-];
-
-const hexColors = [];
-const rgbColors = [];
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
 // Change code below this line
+const {
+  today: {
+    low: lowToday,
+    high: highToday,
+    icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: lowTomorrow,
+    high: highTomorrow,
+    icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+} = forecast;
 
-for (const { hex, rgb } of colors) {
-  hexColors.push(hex);
-  rgbColors.push(rgb);
-}
+// const highToday = forecast.today.high;
+// const lowToday = forecast.today.low;
+// const todayIcon = forecast.today.icon;
+
+// const highTomorrow = forecast.tomorrow.high;
+// const lowTomorrow = forecast.tomorrow.low;
+// const tomorrowIcon = forecast.tomorrow.icon;

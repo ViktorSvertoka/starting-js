bce857efba4e29abeb8f400aedb8e8b5a73a595b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-02 01:59:44 +0200

    update

--- js/script1.js
+++ js/script1.js
@@ -1,15 +1,29 @@
 const atTheOldToad = {
-  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    const potionExists = this.potions.some(
+      potion => potion.name === newPotion.name,
+    );
+    if (potionExists) {
+      return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    } else {
+      this.potions.push(newPotion);
+    }
+  },
+  removePotion(potionName) {
+    this.potions = this.potions.filter(potion => potion.name !== potionName);
+  },
   updatePotionName(oldName, newName) {
-    // Change code below this line
-
-    this.potions = this.potions.map(potion => {
-      if (potion === oldName) {
-        return newName;
-      }
-      return potion;
-    });
-
-    // Change code above this line
+    const potion = this.potions.find(potion => potion.name === oldName);
+    if (potion) {
+      potion.name = newName;
+    }
   },
 };

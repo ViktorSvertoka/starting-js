1b6720701513170aaafd8183171deab550f830cc
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-01 22:57:02 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,17 +1,1 @@
-const bookShelf = {
-  // Change code below this line
-  books: ['The last kingdom', 'The guardian of dreams'],
-  getBooks() {
-    return 'Returning all books';
-  },
-  addBook(bookName) {
-    return `Adding book ${bookName}`;
-  },
-  removeBook(bookName) {
-    return `Deleting book ${bookName}`;
-  },
-  updateBook(oldName, newName) {
-    return `Updating book ${oldName} to ${newName}`;
-  },
-  // Change code above this line
-};
+

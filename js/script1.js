f43fcfa3a85a8f6f7a1ede70cd7ed1d0d1ff5e15
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-02 22:54:54 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,29 +1,7 @@
-const pizzaPalace = {
-  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
-  order(pizzaName, onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return onSuccess(pizzaName);
-    } else {
-      return onError(
-        `There is no pizza with a name ${pizzaName} in the assortment.`,
-      );
-    }
-  },
-};
-// Change code above this line
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+  orderedItems.forEach(item => (totalPrice += item));
+
+  return totalPrice;
 }
-
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order('Smoked', makePizza, onOrderError);
-pizzaPalace.order('Four meats', makePizza, onOrderError);
-pizzaPalace.order('Big Mike', makePizza, onOrderError);
-pizzaPalace.order('Vienna', makePizza, onOrderError);

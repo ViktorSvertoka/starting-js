56156dfbe7ad9861f7ce499be886a83470fc220a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-02 20:58:03 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,15 @@
-function makePizza() {
-  return 'Your pizza is being prepared, please wait.';
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
 }
-// Change code below this line
 
-const result = makePizza();
-const pointer = makePizza;
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+
+console.log(makeMessage('Royal Grand', makePizza));
+console.log(makeMessage('Ultracheese', deliverPizza));

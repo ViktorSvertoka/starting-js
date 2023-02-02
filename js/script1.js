2a0869c193c06a0d2c7c6b288bca63bd591b840f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-02 17:58:55 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,29 +1,7 @@
-const atTheOldToad = {
-  potions: [
-    { name: 'Speed potion', price: 460 },
-    { name: 'Dragon breath', price: 780 },
-    { name: 'Stone skin', price: 520 },
-  ],
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-    const potionExists = this.potions.some(
-      potion => potion.name === newPotion.name,
-    );
-    if (potionExists) {
-      return `Error! Potion ${newPotion.name} is already in your inventory!`;
-    } else {
-      this.potions.push(newPotion);
-    }
-  },
-  removePotion(potionName) {
-    this.potions = this.potions.filter(potion => potion.name !== potionName);
-  },
-  updatePotionName(oldName, newName) {
-    const potion = this.potions.find(potion => potion.name === oldName);
-    if (potion) {
-      potion.name = newName;
-    }
-  },
-};
+function makePizza() {
+  return 'Your pizza is being prepared, please wait.';
+}
+// Change code below this line
+
+const result = makePizza();
+const pointer = makePizza;

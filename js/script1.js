2efd64baaf26d2e0153b000c048f65902ab8903a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-02 22:10:24 +0200

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,29 @@
-function makePizza(pizzaName, callback) {
-  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
-  callback(pizzaName);
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    } else {
+      return onError(
+        `There is no pizza with a name ${pizzaName} in the assortment.`,
+      );
+    }
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
 }
 
-makePizza('Royal Grand', function deliverPizza(pizzaName) {
-  console.log(`Delivering pizza ${pizzaName}.`);
-});
-// Change code below this line
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
 
-makePizza('Ultracheese', function eatPizza(pizzaName) {
-  console.log(`Eating pizza ${pizzaName}.`);
-});
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);

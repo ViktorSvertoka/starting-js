5700dee2d4b344be3a149df43c6ccbd6548566ad
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-03 12:06:54 +0200

    callback function

--- js/script1.js
+++ js/script1.js
@@ -16,6 +16,20 @@ doMath(2, 3, function (x, y) {
   return x + y;
 });
 
-doMath(10, 8, function (x, y) {
-  return x - y;
-});
+doMath(
+  10,
+  8,
+  function (x, y) {
+    return x - y;
+  },
+  function (x, y) {
+    return x - y;
+  },
+  function (x, y) {
+    return x - y;
+  },
+);
+
+// можно внутрь вкладывать сколько угодно функций
+
+//функция это значение, как число, строка и тд

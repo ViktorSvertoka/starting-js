798fcbaf104173c6fa2443e50d6af6a75911b94a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-03 12:00:34 +0200

    callback function

--- js/script1.js
+++ js/script1.js
@@ -4,13 +4,18 @@ const doMath = function (a, b, callback) {
   console.log(result);
 };
 
-const add = function (x, y) {
+// const add = function (x, y) {
+//   return x + y;
+// };
+
+// const sub = function (x, y) {
+//   return x - y;
+// };
+
+doMath(2, 3, function (x, y) {
   return x + y;
-};
+});
 
-const sub = function (x, y) {
+doMath(10, 8, function (x, y) {
   return x - y;
-};
-
-doMath(2, 3, add);
-doMath(10, 8, sub);
+});
